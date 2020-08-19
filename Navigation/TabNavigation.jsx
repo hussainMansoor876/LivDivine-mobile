58719a7010aca7d239c547d258c8afd4c9b1273f
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-08-19 15:51:08 +0500

    admin

--- Navigation/TabNavigation.jsx
+++ Navigation/TabNavigation.jsx
@@ -2,14 +2,22 @@ import * as React from 'react';
 import { Text, View, SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { Button } from 'react-native-elements'
 import { ApprovedAdvisors, PendingAdvisors } from '../Screens/Admin'
+import { appColor } from '../utils/constant'
+import { color } from 'react-native-reanimated';
 const Tab = createMaterialTopTabNavigator();
 
 export default function App() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
+      <Button
+        title="Logout"
+        buttonStyle={{ backgroundColor: appColor }}
+      />
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+        >
           <Tab.Screen name="Approved" component={ApprovedAdvisors} />
           <Tab.Screen name="Pending" component={PendingAdvisors} />
         </Tab.Navigator>
