561be23bfa90475004a2ae92b876518326ee2c61
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-08-22 21:27:32 +0500

    done

--- Screens/Home/Home.jsx
+++ Screens/Home/Home.jsx
@@ -81,6 +81,26 @@ const Home = (props) => {
                             <Text style={{ marginLeft: 30, marginTop: 170, flexDirection: 'row' }}>2020</Text>
                             <Text style={{ marginLeft: -35, marginTop: 185, flexDirection: 'row' }}>Year joined</Text> */}
                     </View>
+                    <View style={homeStyles.childStyle}>
+                        <Image
+                            style={homeStyles.tile}
+                            source={{ uri: dummyImage }}
+                        />
+                        <Text style={homeStyles.name}>Yasir Khan</Text>
+                        <Text style={homeStyles.ctgry}>Love & Career</Text>
+                        <View style={{ flexDirection: 'row' }}>
+                            <Rating
+                                imageSize={20}
+                                style={homeStyles.rating}
+                                startingValue={5}
+                            />
+                            <Text style={{ ...homeStyles.rating, marginLeft: 15 }}>5</Text>
+                        </View>
+                        {/* <Text style={{ marginLeft: -60, marginTop: 170, flexDirection: 'row' }}>1,123</Text>
+                            <Text style={{ marginLeft: -45, marginTop: 185, flexDirection: 'row' }}>Readings</Text>
+                            <Text style={{ marginLeft: 30, marginTop: 170, flexDirection: 'row' }}>2020</Text>
+                            <Text style={{ marginLeft: -35, marginTop: 185, flexDirection: 'row' }}>Year joined</Text> */}
+                    </View>
                 </View>
             </ScrollView>
             <Modal
