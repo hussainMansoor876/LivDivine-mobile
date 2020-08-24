1a214c3106e0338116c61e8de36fe59457522bb0
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-08-24 18:17:23 +0500

    done

--- Screens/AdvisorSettings/AdvisorSettings.jsx
+++ Screens/AdvisorSettings/AdvisorSettings.jsx
@@ -1,25 +1,48 @@
-import React from 'react';
-import { SafeAreaView, ScrollView, Text, Button, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, ScrollView, Text, View } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { loginUser, removeUser } from '../../Redux/actions/authActions';
 import { ASettingsForm, ChangePassword, OrderTypeUpdate } from '../../Components'
 import { loginStyles, settingsStyles } from '../../styles'
-
-
-
+import { Button } from 'react-native-elements'
 
 const AdvisorSettings = (props) => {
     const user = useSelector(state => state.authReducer.user);
     const dispatch = useDispatch();
+    const [state, setState] = useState({
+        showOrder: false,
+        showCategory: false
+    })
+
+    const updateField = (obj) => {
+        setState({
+            ...state,
+            ...obj
+        })
+    }
+
+
     return (
         <SafeAreaView style={loginStyles.setFlex}>
             <ScrollView style={loginStyles.setFlex}>
                 <View style={settingsStyles.header}>
                     <Text h1 style={settingsStyles.fieldsbold}>ADVISOR ACCOUNT</Text>
                 </View>
-                <OrderTypeUpdate {...props} />
-                <ASettingsForm {...props} />
-                {user.authType === null ? <ChangePassword {...props} /> : null}
+                {state.showOrder ? <OrderTypeUpdate {...props} /> : <View>
+                    <View style={{ marginTop: 10, marginBottom: -10, flexDirection: 'row', justifyContent: 'space-around' }}>
+                        <Button
+                            title="Orders Setting"
+                            buttonStyle={{ ...loginStyles.loginBtn, width: 180 }}
+                            onPress={() => updateField({ showOrder: true })}
+                        />
+                        <Button
+                            title="Categories Setting"
+                            buttonStyle={{ ...loginStyles.loginBtn, width: 180 }}
+                        />
+                    </View>
+                    <ASettingsForm {...props} />
+                    {user.authType === null ? <ChangePassword {...props} /> : null}
+                </View>}
             </ScrollView>
         </SafeAreaView>
     );
