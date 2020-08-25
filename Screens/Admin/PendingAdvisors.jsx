3662d6611e51bca750f2e01ea8c0108dc6790fb2
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-08-25 21:31:00 +0500

    done

--- Screens/Admin/PendingAdvisors.jsx
+++ Screens/Admin/PendingAdvisors.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { SafeAreaView, ScrollView, Text, View, Alert, ActivityIndicator, TouchableOpacity } from 'react-native';
-import { connect } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, ScrollView, Text, View, Alert, ActivityIndicator, TouchableOpacity, Image } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
 import { loginUser, removeUser } from '../../Redux/actions/authActions';
 import { loginStyles, AdvisorStyles } from '../../styles'
 import { Icon, Button, ListItem } from 'react-native-elements'
@@ -10,57 +10,50 @@ import Spinner from 'react-native-loading-spinner-overlay';
 import client from '../../Config/apollo'
 
 
+const PendingAdvisors = (props) => {
+    const user = useSelector(state => state.authReducer.user);
+    const dispatch = useDispatch()
+    const [isLoading, setLoading] = useState(false)
+    const [updateList, setUpdateList] = useState(true)
+    const [spin, setSpin] = useState(false)
+    let [allAdvisors, setAllAdvisors] = useState([])
 
-class PendingAdvisors extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isLoading: true,
-            allAdvisors: [],
-            updateList: true,
-            spin: false,
-        }
-    }
-
-    getData = () => {
+    useEffect(() => {
         client.query({ variables: { adminId: '421c6267-7911-4686-91fe-fe424e8efe00', isApproved: false }, query: GET_ADVISORS })
             .then((res) => {
                 const { getAllAdvisorForAdmin } = res.data
                 if (getAllAdvisorForAdmin?.user?.length) {
-                    this.setState({
-                        allAdvisors: getAllAdvisorForAdmin.user,
-                        isLoading: false
-                    })
+                    setAllAdvisors(getAllAdvisorForAdmin.user)
                 }
-                else {
-                    this.setState({
-                        isLoading: false
-                    })
+                setLoading(false)
+            })
+    })
+
+    const getData = () => {
+        client.query({ variables: { adminId: '421c6267-7911-4686-91fe-fe424e8efe00', isApproved: false }, query: GET_ADVISORS })
+            .then((res) => {
+                const { getAllAdvisorForAdmin } = res.data
+                if (getAllAdvisorForAdmin?.user?.length) {
+                    setAllAdvisors(getAllAdvisorForAdmin.user)
                 }
+                setLoading(false)
             })
             .catch((e) => {
                 Alert.alert('Oops Something Went Wrong!')
-                this.setState({ isLoading: false })
+                setLoading(false)
             })
     }
 
-    componentDidMount() {
-        const { user } = this.props
-        // if (!user) {
-        //     this.props.history.replace('/login')
-        // }
-        this.getData()
-    }
-
-    updateStatus = (userId, status) => {
-        this.setState({ spin: true })
+    const updateStatus = (userId, status) => {
+        setSpin(true)
         client.mutate({ variables: { userId, adminId: '421c6267-7911-4686-91fe-fe424e8efe00', status }, mutation: UPDATE_STATUS })
             .then((res) => {
                 const { approvedAdvisor } = res.data
                 if (approvedAdvisor.success) {
-                    this.setState({ allAdvisors: [], isLoading: true, spin: false }, () => {
-                        this.getData()
-                    })
+                    setAllAdvisors([])
+                    setLoading(true)
+                    setSpin(false)
+                    getData()
                 }
                 else {
                     Alert.alert('Oops Something Went Wrong!')
@@ -73,66 +66,69 @@ class PendingAdvisors extends React.Component {
             })
     }
 
-    render() {
-        const { allAdvisors, isLoading, spin } = this.state
-
-        return (
-            <SafeAreaView style={loginStyles.setFlex}>
-                <Spinner
-                    visible={spin}
-                    textContent={'Loading...'}
-                    textStyle={loginStyles.spinnerTextStyle}
+    return (
+        <SafeAreaView style={loginStyles.setFlex}>
+            <Spinner
+                visible={spin}
+                textContent={'Loading...'}
+                textStyle={loginStyles.spinnerTextStyle}
+            />
+            {allAdvisors.map((v, i) => {
+                return (
+                    <View key={i} style={{ flexDirection: 'row', paddingTop: 10, paddingBottom: 10, backgroundColor: '#fff', borderBottomColor: 'rgba(0, 0, 0, 0.5)', borderBottomWidth: 0.5, justifyContent: 'space-between' }}>
+                        <View style={{ flexDirection: 'row' }}>
+                            <Image
+                                source={{ uri: v.image }}
+                                style={{ width: 50, height: 50, marginRight: 10, marginLeft: 10, borderRadius: 250 }}
+                            />
+                            <Text style={{ fontSize: 16, marginTop: 12 }}>{v.userName}</Text>
+                        </View>
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: 150, marginRight: 10 }}>
+                            <Button
+                                title="Approve"
+                                onPress={() => updateStatus(item.id, true)}
+                            />
+                            <Button
+                                title="Cancel"
+                                onPress={() => updateStatus(item.id, false)}
+                                buttonStyle={{ backgroundColor: '#d9534f' }}
+                            />
+                        </View>
+                    </View>
+                )
+            })}
+            {/* {allAdvisors.map((item, i) => (
+                <ListItem
+                    key={i}
+                    title={item.userName}
+                    leftAvatar={{ source: { uri: item.image } }}
+                    bottomDivider
+                    chevron={
+                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: 150 }}>
+                            <Button
+                                title="Approve"
+                                onPress={() => updateStatus(item.id, true)}
+                            />
+                            <Button
+                                title="Cancel"
+                                onPress={() => updateStatus(item.id, false)}
+                                buttonStyle={{ backgroundColor: '#d9534f' }}
+                            />
+                        </View>
+                    }
                 />
-                {allAdvisors.length ? allAdvisors.map((item, i) => {
-                    return (
-                        <ListItem
-                            key={i}
-                            title={item.userName}
-                            leftAvatar={{ source: { uri: item.image } }}
-                            bottomDivider
-                            chevron={
-                                <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: 150 }}>
-                                    <Button
-                                        title="Approve"
-                                        onPress={() => this.updateStatus(item.id, true)}
-                                    />
-                                    <Button
-                                        title="Cancel"
-                                        onPress={() => this.updateStatus(item.id, false)}
-                                        buttonStyle={{ backgroundColor: '#d9534f' }}
-                                    />
-                                </View>
-                            }
-                        />
-                    )
-                }
-                ) : null}
-                {
-                    isLoading && !allAdvisors.length ? <ActivityIndicator
-                        color="rgba(0, 0, 0, 0.5)"
-                        size="small"
-                        style={AdvisorStyles.activityStyle}
-                    /> : !isLoading && !allAdvisors.length ? <View style={AdvisorStyles.container}>
-                        <Text>No Advisor found!</Text>
-                    </View> : null
-                }
-            </SafeAreaView >
-        );
-    }
-}
+            ))} */}
+            {
+                isLoading && !allAdvisors.length ? <ActivityIndicator
+                    color="rgba(0, 0, 0, 0.5)"
+                    size="small"
+                    style={AdvisorStyles.activityStyle}
+                /> : !isLoading && !allAdvisors.length ? <View style={AdvisorStyles.container}>
+                    <Text>No Advisor found!</Text>
+                </View> : null
+            }
+        </SafeAreaView >
+    );
+};
 
-const mapStateToProps = (state) => {
-    return {
-        user: state.authReducer.user,
-    }
-}
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        loginUser: (user) => dispatch(loginUser(user)),
-    }
-}
-
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(PendingAdvisors)
+export default PendingAdvisors;
