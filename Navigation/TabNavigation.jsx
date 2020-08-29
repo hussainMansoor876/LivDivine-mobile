eee412c7ff74a973568bc8affddb9a4138bd936b
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-08-30 00:13:17 +0500

    done

--- Navigation/TabNavigation.jsx
+++ Navigation/TabNavigation.jsx
@@ -1,22 +1,26 @@
-import * as React from 'react';
+import React from 'react';
 import { Text, View, SafeAreaView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { Button } from 'react-native-elements'
+import { useSelector, useDispatch } from 'react-redux';
 import { ApprovedAdvisors, PendingAdvisors } from '../Screens/Admin'
 import { appColor } from '../utils/constant'
+import { removeUser } from '../Redux/actions/authActions'
 const Tab = createMaterialTopTabNavigator();
 
-export default function App() {
+const TabScreen = () => {
+  const dispatch = useDispatch();
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Button
         title="Logout"
+        onPress={() => dispatch(removeUser())}
         buttonStyle={{ backgroundColor: appColor, alignSelf: 'flex-end', margin: 20, width: 120 }}
       />
       <NavigationContainer>
         <Tab.Navigator
-        lazy={true}
+          lazy={true}
         >
           <Tab.Screen name="Approved" component={ApprovedAdvisors} />
           <Tab.Screen name="Pending" component={PendingAdvisors} />
@@ -25,3 +29,5 @@ export default function App() {
     </SafeAreaView>
   );
 }
+
+export default TabScreen
