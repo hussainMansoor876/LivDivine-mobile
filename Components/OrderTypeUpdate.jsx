6628dd4ed2e9bb57497d3175bdb9a7809dbd9507
hussainMansoor876 <hussainMansoor876@gmail.com>
2020-08-30 02:25:46 +0500

    done

--- Components/OrderTypeUpdate.jsx
+++ Components/OrderTypeUpdate.jsx
@@ -7,13 +7,15 @@ import client from '../Config/apollo'
 import { loginStyles, settingsStyles } from '../styles'
 import Spinner from 'react-native-loading-spinner-overlay';
 import { UPDATE_PASSWORD } from '../utils/authQueries'
-import { appColor, roundToTwo } from '../utils/constant';
+import { orderTypesCopy, appColor, roundToTwo } from '../utils/constant';
 import Screen from '../utils/ScreenDimensions'
 import Slider from 'react-native-slider'
 
 const SettingsForm = (props) => {
     const dispatch = useDispatch();
     const user = useSelector(state => state.authReducer.user);
+    let [orderTypes, setOrdertypes] = useState(orderTypesCopy)
+    const [orderUpdate, setOrderUpdate] = useState(false)
     const [state, setState] = useState({
         isLoading: false,
         sliderValue: 0.5
@@ -61,6 +63,17 @@ const SettingsForm = (props) => {
         })
     }
 
+    const updateOrders = (i, value) => {
+        orderTypes[i].price = value
+        setOrderUpdate(!orderUpdate)
+        // setOrdertypes(orderTypes)
+    }
+
+    const updateActive = (i) =>{
+        orderTypes[i].isActive = !orderTypes[i].isActive
+        setOrderUpdate(!orderUpdate)
+    }
+
     return (
         <View style={{ ...settingsStyles.aSettingsView, marginBottom: 30 }}>
             <Spinner
@@ -75,50 +88,33 @@ const SettingsForm = (props) => {
             />
             <Text style={settingsStyles.textStyle}>Update Orders</Text>
             <View style={{ width: '95%', marginLeft: '2%' }}>
-                <View style={{ marginBottom: 5, marginTop: 5, borderTopColor: '#000000', borderTopWidth: 0.5 }}>
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                        <Text style={{ fontSize: 20, letterSpacing: 1.2, marginTop: 10 }}>Video Reading</Text>
-                        <CheckBox
-                            title="Activate"
-                            checked
-                        />
-                    </View>
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                        <Slider
-                            value={state.sliderValue}
-                            maximumValue={100}
-                            minimumValue={0.1}
-                            onValueChange={(value) => updateField({ sliderValue: value })}
-                            step={0.1}
-                            minimumTrackTintColor={appColor}
-                            thumbTintColor={appColor}
-                            style={{ width: Screen.width - 90 }}
-                        />
-                        <Text style={{ fontSize: 18 }}>${roundToTwo(state.sliderValue)}</Text>
-                    </View>
-                </View>
-                <View>
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                        <Text style={{ fontSize: 20, letterSpacing: 1.2, marginTop: 10 }}>Video Reading</Text>
-                        <CheckBox
-                            title="Activate"
-                            checked
-                        />
-                    </View>
-                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                        <Slider
-                            value={state.sliderValue}
-                            maximumValue={100}
-                            minimumValue={0.1}
-                            onValueChange={(value) => updateField({ sliderValue: value })}
-                            step={0.1}
-                            minimumTrackTintColor={appColor}
-                            thumbTintColor={appColor}
-                            style={{ width: Screen.width - 90 }}
-                        />
-                        <Text style={{ fontSize: 18 }}>${roundToTwo(state.sliderValue)}</Text>
-                    </View>
-                </View>
+                {orderTypes.map((v, i) => {
+                    return (
+                        <View key={i} style={{ marginBottom: 5, marginTop: 5, borderTopColor: '#000000', borderTopWidth: 0.5 }}>
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                                <Text style={{ fontSize: 20, letterSpacing: 1.2, marginTop: 10 }}>{v.orderTypeName}</Text>
+                                <CheckBox
+                                    title="Activate"
+                                    checked={v.isActive}
+                                    onPress={() => updateActive(i)}
+                                />
+                            </View>
+                            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                                <Slider
+                                    value={v.price}
+                                    maximumValue={100}
+                                    minimumValue={0.1}
+                                    onValueChange={(value) => updateOrders(i, value)}
+                                    step={0.1}
+                                    minimumTrackTintColor={appColor}
+                                    thumbTintColor={appColor}
+                                    style={{ width: Screen.width - 90 }}
+                                />
+                                <Text style={{ fontSize: 18 }}>${roundToTwo(v.price)}</Text>
+                            </View>
+                        </View>
+                    )
+                })}
             </View>
             <Button
                 title="UPDATE ORDERS"
